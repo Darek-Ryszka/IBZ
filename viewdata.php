7228a2655f3fc4b1a31a0e0d41d596409a4ed817
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-11-07 12:43:01 +0100

    3.4

--- viewdata.php
+++ viewdata.php
@@ -168,133 +168,52 @@
             // Widok numer 5
             echo "<h4>5. Widok wyświetlający tylko istniejących użytkowników (bez korzystania z tabelki subscribers)</h4>";
             try{
-                // $sql = "SELECT subscriber_name FROM audit_subscribers";
-                // // $sql = "SELECT * FROM audit_subscribers WHERE action_performed='Insert a new subscriber'";  
-                // // $sql = "SELECT subscriber_name FROM audit_subscribers WHERE action_performed='Insert a new subscriber' OR action_performed='Updated a subscriber'";  
-                // $result = $pdo->query($sql);
-                // if($result->rowCount() > 0){
-                //     echo "<table>";
-                //         echo "<tr>";
-                //             // echo "<th>id</th>";
-                //             echo "<th>User name</th>";
-                //         echo "</tr>";
-                //         while($row = $result->fetch()){
-                //             echo "<tr>";
-                //                 // echo "<td>" . $row['id'] . "</td>";
-                //                 echo "<td>" . $row['subscriber_name'] . "</td>";
-                //             echo "</tr>";  
-                //         }  
-                //     echo "</table>";
-                //     unset($result);
-                // } else{
-                //     echo "Brak rekordów w bazie danych do wyświetlenia.";
-                // }
+                    $sql1 = "SELECT subscriber_name AS 'in' FROM audit_subscribers WHERE action_performed='Insert a new subscriber'"; 
 
-                $sql1 = "SELECT subscriber_name AS 'in' FROM audit_subscribers WHERE action_performed='Insert a new subscriber'"; 
+                    $sql2 = "SELECT subscriber_name AS 'dn' FROM audit_subscribers WHERE action_performed='Deleted a subscriber'";
+                    // OR action_performed='Updated a subscriber'
 
-                $sql2 = "SELECT subscriber_name AS 'dn' FROM audit_subscribers WHERE action_performed='Deleted a subscriber'";
-                // OR action_performed='Updated a subscriber'
-
-                $result1 = $pdo->query($sql1);
-                $result2 = $pdo->query($sql2);   
-
-                // $i=0;
-                // while($row1 = $result1->fetch())
-                // {
-                //     $row1['in'] = $tab[$i];
-                //     $i++
-                // };
-                
-                if($result1->rowCount() > 0 ){
-                    echo "<table>";
-                    echo "<tr>";
-                        echo "<th>User name</th>";
-                    echo "</tr>";
-                    // while($row1 = $result1->fetch()){
-                    //     while($row2 = $$result2->fetch()){
-                    //         if($row1['subscriber_name'] != $row2['subscriber_name']){
-                    //             echo "<tr>";
-                    //                 echo "<td>" . $row1['subscriber_name'] . "</td>";
-                    //             echo "</tr>";  
-                    //             }else {
-                    //                 continue;
-                    //             }
-
-                    //         // echo "<tr>";
-                    //         //     echo "<td>" . $row1['in'] . "</td>";
-                    //         // echo "</tr>";
-                    //         // for($i=0; $i<=$result2->num_rows;$i++)    
-                    //         while($row2 = $result2->fetch()){
-                    //         if($row1['in'] == $row2['dn']){
-                    //         echo "<tr>";
-                    //             echo "<td>" . $row1['in'] . "</td>";
-                    //         echo "</tr>";
-                    //         break;
-                    //         }
-                    //     }    
-                    // }  
-
-                // $rows1 = $result1->fetchAll();
-                // print_r($rows1);    
-                // // $temp = explode(", ", $rows1);
-                
-                // $rows2 = $result2->fetchAll();
-                // print_r($rows2);        
-
-                // array_intersect($rows1, $rows2);
-                // echo "<tr>";
-                //     echo "<td>" . $row1['in'] . "</td>";
-                // echo "</tr>";
-                // $result = array_diff($rows1, $rows2);
-                // print_r($result);
-
-
-                    // for($i=1;$i<=$result1->fetch();$i++)
-                    // {
-                    //     $row1=$result1->fetch();
-
-                    //     for($j=1;$j<=$result2->fetch();$j++)
-                    //     {
-                    //         $row2=$result2->fetch();
-
-                    //         if($row1['in'] == $row2['dn'])
-                    //         {
-                    //             echo "<tr>";
-                    //                 echo "<td>" . $row1['in'] . "</td>";
-                    //             echo "</tr>";
-                    //             break;
-                    //         }
-                    //     }
-                    // }
-
-                    while($row1 = $result1->fetch())
-                    {
-                        while($row2 = $result2->fetch())
-                        {
-                            if($row1['in'] != $row2['dn'])
-                            {
-                                echo "<tr>";
-                                    echo "<td>" . $row1['in'] . "</td>";
-                                echo "</tr>";
-                                break;
-                            }
-                            else
-                            {
-                                continue;
-                            }
-                        }    
-                    }  
-                        echo "</table>";
-                        unset($result1, $result2 );
-                    } else{
-                        echo "Brak rekordów w bazie danych do wyświetlenia.";
-                    }
+                    $result1 = $pdo->query($sql1);
+                    $result2 = $pdo->query($sql2);   
                     
-                }       
-            catch(PDOException $e){
-                die("Błąd! Nie można wprowadzić danych $sql. " . $e->getMessage());
-            }
+                    if($result1->rowCount() > 0 )
+                    {
+                        echo "<table>";
+                        echo "<tr>";
+                            echo "<th>User name</th>";
+                        echo "</tr>";                   
 
+                        while($row1 = $result1->fetch())
+                        {
+                            echo "<tr>";
+                                echo "<td>" . $row1['in'] . "</td>";
+                                // echo "<td>" . $row2['dn'] . "</td>";
+                            echo "</tr>";
+                            while($row2 = $result2->fetch())
+                            { 
+                                // if($row2['dn'])                               
+                                if($row1['in'] != $row2['dn'])
+                                {
+                                    echo "<tr>";
+                                        echo "<td>" . $row1['in'] . "</td>";
+                                        echo "<td>" . $row2['dn'] . "</td>";
+                                    echo "</tr>";                                    
+                                    break;
+                                }                              
+                            }    
+                        }  
+                            echo "</table>";
+                            unset($result1, $result2 );
+                    } 
+                    else
+                    {
+                        echo "Brak rekordów w bazie danych do wyświetlenia.";
+                    }                    
+                }       
+                catch(PDOException $e)
+                {
+                    die("Błąd! Nie można wprowadzić danych $sql. " . $e->getMessage());
+                }
             // Zamknięcie połączenia z bazą danych
             unset($pdo);
         ?>
